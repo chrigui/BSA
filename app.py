86611323913c506c1cb2518f3e6e6029dc3a2ec0
T Chrigui <chrigui@users.noreply.github.com>
2024-03-31 17:27:22 +0300

    Update and rename Home.py to app.py

--- app.py
+++ app.py
@@ -282,3 +282,10 @@ if uploaded_file is not None:
                 st.plotly_chart(fig)
         else:
             st.write("Please select at least one numeric column.")
+            hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
